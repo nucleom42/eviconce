8778939b6cf18648b9ce0c0a48485c629c9000ec
Oleg <oleg.saltykov@gmail.com>
2026-04-25 18:09:55 -0400

    update docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-# Run initialization and migrations
+# Wait for DB to be ready (even with healthchecks, Postgres can take a second)
+sleep 2 
+
+# If db:init needs the environment, make sure rubee loads it
 bundle exec rubee db:init
 bundle exec rubee db:run:all
 
-# Then run the main command (rubee start)
 exec "$@"
